08821118d1b8aabf554f29dfa6dcd28d812d3b88
unknown <NiczinXD>
2025-06-21 09:36:30 -0300

    03. Persistindo dados

--- src/app.js
+++ src/app.js
@@ -1,31 +1,27 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+})
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco sendo feita com sucesso")
+})
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id)
-    })
-}
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({})
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
@@ -51,3 +47,5 @@ app.delete("/livros/:id", (req, res) => {
 });
 
 export default app;
+
+//mongodb+srv://admin:<db_password>@cluster0.1oel1vc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
